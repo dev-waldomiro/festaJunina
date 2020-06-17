fd7efc41e5a2d2af61ff78e4c1a7f9d69d1c3f18
Leobrine25 <55628848+Leobrine25@users.noreply.github.com>
2020-06-17 17:00:47 -0300

    Camera
    Camera segue jogador, pecisando de um pouco de melhora para ficar "mais liso"

---
+++ projetoJunino/Assets/Scrips/Camera_Follow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    [Range(0, 20)]
+    public float smoother = 0.125f;
+    public Vector3 compensador;
+
+    void FixedUpdate()
+    {
+        Vector3 posicaoDesejada = target.position + compensador;
+        Vector3 posicaoLisa = Vector3.Lerp(transform.position, posicaoDesejada, smoother * Time.deltaTime);
+        transform.position = posicaoLisa;
+    }
+}
